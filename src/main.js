b5cb4c41ceff146e6d76d3ea8ad47ce96b458e81
Matheus Silva <rematheussilva@gmail.com>
2026-01-31 15:05:27 -0500

    Evento de carregamento de conteúdo

--- src/main.js
+++ src/main.js
@@ -8,3 +8,4 @@ import "./styles/global.css";
 import "./styles/schedule.css";
 
 import "./modules/form/submit.js";
+import "./modules/load.js";
---
+++ src/modules/load.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("O DOM esta pronto!");
+});
