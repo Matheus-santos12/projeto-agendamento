5d3e6e2b2cdfc13e75d443001526873c886d6429
Matheus Silva <rematheussilva@gmail.com>
2026-01-31 21:07:02 -0500

    Envia o horário selecionado para a APi

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -2,6 +2,7 @@ import dayjs from "dayjs";
 
 const form = document.querySelector("form");
 const selectedDate = document.getElementById("date");
+const clientName = document.getElementById("client");
 
 const inputToday = dayjs(new Date()).format("YYYY-MM-DD");
 
@@ -12,5 +13,32 @@ selectedDate.min = inputToday;
 form.onsubmit = (event) => {
   event.preventDefault();
 
-  console.log("Enviado..");
+  try {
+    // Recuperando o nome do cliente.
+    const name = clientName.value;
+
+    if (!name) {
+      return alert("Por favor, informe o nome do cliente");
+    }
+
+    // Recupera o horário selecionado.
+    const hourSelected = document.querySelector(".hour.selected");
+
+    if (!hourSelected) {
+      return alert("Selecione um horário para o agendamento.");
+    }
+
+    // Recupera somente a hora.
+    const [hour] = hourSelected.innerText.split(":");
+
+    // Insere a hora da meta.
+    const when = dayjs(selectedDate.value).add(hour, "hour");
+
+    const id = new Date().getTime();
+  } catch (error) {
+    alert(
+      "Não foi possível realizar o agendamento. Por favor tente mais tarde.",
+      console.log(error),
+    );
+  }
 };
